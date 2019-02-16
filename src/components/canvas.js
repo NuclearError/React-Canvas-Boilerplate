c80c6a24a7f135f6000ccd2c182315e0be4df6c4
Angela Cullen <esmearmitage@gmail.com>
2019-02-16 11:43:08 +0000

    added basic old-school image loading logic

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -2,9 +2,20 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 const canvasStyle = {
+  margin: '20px',
   border: '1px dotted',
-  background: 'thistle',
+  background: 'azure',
 };
+
+const allGameImageUrls = [
+  'http://i61.tinypic.com/2i95fzb.png', /* map sprite */
+  'http://i62.tinypic.com/2its8ra.png', /* clown sprite */
+  'http://i59.tinypic.com/xpyjr9.png', /* centaur sprite */
+  'https://i.imgur.com/T49YqUc.png', /* crab */
+];
+
+const allGameImages = [];
+let assetCount = 0;
 
 class Canvas extends Component {
   constructor(props) {
@@ -15,7 +26,21 @@ class Canvas extends Component {
   }
 
   componentDidMount() {
-    this.setupCanvas();
+    // loading the image file now rather than waiting later
+    // - for bigger projects, set up a real system for preloading assets
+
+    for (let i = 0; i < allGameImageUrls.length; i++) {
+      const img = new Image();
+      img.onload = () => {
+        assetCount++;
+        if (assetCount === allGameImageUrls.length) {
+          console.log('All the images finished loading: ', allGameImages);
+          this.setupCanvas();
+        }
+      };
+      img.src = allGameImageUrls[i];
+      allGameImages.push(img);
+    }
   }
 
   setupCanvas() {
@@ -26,11 +51,16 @@ class Canvas extends Component {
     stage.textBaseline = 'top';
     stage.fillStyle = '#000';
     stage.lineWidth = 2;
+    this.renderImage(stage);
     this.renderMessage(stage, this.props.message);
   }
 
   renderMessage(stage, message) {
-    stage.fillText(message, 100, 100);
+    stage.fillText(message, 300, 100);
+  }
+
+  renderImage(stage) {
+    stage.drawImage(allGameImages[3], 25, 0);
   }
 
   render() {
